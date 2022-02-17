42c6b2fafc15105329aefaaa4fe81fc29872684b
kasurashan <kasurashan@gmail.com>
2022-02-17 12:12:15 +0900

    Update matcher.py

--- methods/vidt/matcher.py
+++ methods/vidt/matcher.py
@@ -100,19 +100,59 @@ class HungarianMatcher(nn.Module):
             '''
             Cost matrix 나누는 부분
             아직 수정 중
+
+            '''
+
+            indices_total = []
             for i, c in enumerate(C.split(sizes, -1)):
                 LMS = tgt_lms[i]
-                
+
                 LMS = np.array(LMS)
                 small_idx = np.where(LMS == "small")[0].tolist()
                 medium_idx = np.where(LMS == "medium")[0].tolist()
                 large_idx = np.where(LMS == "large")[0].tolist()
 
-                C_small = c[:, :25 , small_idx]
-                C_medium = c[:, 25:75, medium_idx]
-                C_large = c[:, 75:, large_idx]
-            '''
-            indices = [linear_sum_assignment(c[i]) for i, c in enumerate(C.split(sizes, -1))]
+                C_small = c[i, :25 , small_idx]
+                C_medium = c[i, 25:75, medium_idx]
+                C_large = c[i, 75:, large_idx]
+
+                idx_all = small_idx + medium_idx + large_idx
+                
+                idx_dict = dict(zip(range(len(idx_all)), idx_all))
+
+                indices_s_dummy = [linear_sum_assignment(C_small)]
+                idx_s = []
+                for i in indices_s_dummy[0][1]:
+                    idx_s.append(idx_dict[i])
+                idx_s = np.array(idx_s)
+                indices_s = [(indices_s_dummy[0][0] + np.array([25]), idx_s)]
+
+
+                indices_m_dummy = [linear_sum_assignment(C_medium)]
+                idx_m = []
+                for i in indices_m_dummy[0][1]:
+                    idx_m.append(idx_dict[i])
+                idx_m = np.array(idx_m)
+                indices_m = [(indices_m_dummy[0][0] + np.array([25]), idx_m)]
+
+                indices_l_dummy = [linear_sum_assignment(C_large)]
+                idx_l = []
+                for i in indices_l_dummy[0][1]:
+                    idx_l.append(idx_dict[i])
+                idx_l = np.array(idx_l)
+                indices_l = [(indices_l_dummy[0][0] + np.array([25]), idx_l)]
+
+                indices_lms = [(np.concatenate((indices_s[0][0], indices_m[0][0], indices_l[0][0]), axis=0), np.concatenate((indices_s[0][1], indices_m[0][1], indices_l[0][1]), axis=0))]
+                indices_total = indices_total + indices_lms
+
+            indices = indices_total 
+
+
+
+
+
+
+            #indices = [linear_sum_assignment(c[i]) for i, c in enumerate(C.split(sizes, -1))]
             return [(torch.as_tensor(i, dtype=torch.int64), torch.as_tensor(j, dtype=torch.int64)) for i, j in indices]
 
 
